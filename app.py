28b784e8a6ba4b6d58edd08150392712e864d7f4
Brad-Jay <106689518+Brad-Jay@users.noreply.github.com>
2024-11-19 20:45:30 +1100

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.title("ShopWise Genie")
 
 if st.session_state.start_chat:
     if "openai_model" not in st.session_state:
-        st.session_state.openai_model = "gpt-4o-mini"
+        st.session_state.openai_model = "gpt-3.5-turbo"
     if "messages" not in st.session_state:
         st.session_state.messages = []
     
